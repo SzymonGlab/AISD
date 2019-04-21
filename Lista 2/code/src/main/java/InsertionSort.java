35eef541c6bf804c0f85ab6f938d8136353cfb6d
Szymon Głąb <43967199+SzymonGlab@users.noreply.github.com>
2019-04-21 22:09:54 +0200

    Update InsertionSort.java

--- Lista 2/code/src/main/java/InsertionSort.java
+++ Lista 2/code/src/main/java/InsertionSort.java
@@ -1,14 +1,13 @@
 public class InsertionSort extends Sort {
 
   public void sortASC(int array[], int low, int high) {
-    int arraySize = high +1;                                                                                            //rozmiar tablicy to ilość indeksów +1 (high to numer ostantiego indeksu)
+    int arraySize = high +1;                                                                                            
       for (int i=low; i<arraySize;   ++i)
       {
         int key = array[i];
-        int j = i-1;                                                                                                    // indexy posortowanych elementów które musimy porównać z nasszym kluczem
-        while (j>=0 && array[j] > key)                                                                                  // Sprawdza czy poprzedni element jest większy od aktualnie badanego
-                                                                                                                        // jeżeli tak to przesuwa ułożone już elementy w prawo, dopóki są one większe od badanego elementu.
-                                                                                                                        // Potem w odpowiednie miejsce wstawia badany element
+        int j = i-1;                                                                                                    
+        while (j>=0 && array[j] > key)                                                                                  
+                                                                                                                        
         {
           comparisionCounter++;
           swapCounter ++;
@@ -24,15 +23,15 @@ public class InsertionSort extends Sort {
 
 
   public void sortDESC(int array[], int low, int high){
-    int arraySize = high +1;                                                                                            //rozmiar tablicy to ilość indeksów +1 (high to numer ostantiego indeksu)
+    int arraySize = high +1;                                                                                            
     for (int i=1; i<arraySize; ++i)
     {
       int key = array[i];
       int j = i-1;
 
-      while (j>=0 && array[j] < key)                                                                                    // Sprawdza czy poprzedni element jest mniejszy od aktualnie badanego
-                                                                                                                        // jeżeli tak to przesuwa ułożone już elementy w prawo, dopóki są one większe od badanego elementu.
-                                                                                                                        // Potem w odpowiednie miejsce wstawia badany element.
+      while (j>=0 && array[j] < key)                                                                                    
+                                                                                                                        
+                                                                                                                        
       {
 //        System.err.println("COMPARITION");
 //        System.err.println("SWAP");
